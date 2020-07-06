eafb0a2cd50ce5e78e4c1de253f81d0a19af9ef7
JanSpaepen <jan.spaepen@gmail.com>
2020-07-06 12:15:15 +0200

    Settings Section Part 1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,15 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Request;
-use Response;
+
+
 use Illuminate\Support\Arr;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+
+
 
 class Handler extends ExceptionHandler
 {
@@ -33,10 +36,12 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -45,10 +50,12 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
@@ -58,6 +65,7 @@ class Handler extends ExceptionHandler
      * @param AuthenticationException $exception
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
+
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
